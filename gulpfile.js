bf4a04e3982b39b5d8635a4a1778d46f55454ae6
NadimRai <rai_nadim@yahoo.com>
2017-02-13 12:10:43 -0800

    concat js files in a single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./build/js'));
-});
+// gulp.task('jsBrowserify', function() {
+//   return browserify({ entries: ['./js/pingpong-interface.js'] })
+//     .bundle()
+//     .pipe(source('app.js'))
+//     .pipe(gulp.dest('./build/js'));
+// });
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
